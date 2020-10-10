656dce22d893eafec171eb953fa43da17565e7b4
Dennis HG <dennishg32@gmail.com>
2020-10-10 13:23:55 +0200

    additional of function of toppings and size, crust in js file

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
+
+function Size(size, price) {
+    this.size = size;
+    this.price = price;
+}
+
+function Crust(name) {
+    this.name = name;
+    this.sizes = [];
+}
+
+function Toppings(name) {
+    this.name = name;
+    this.sizes = [];
+}
