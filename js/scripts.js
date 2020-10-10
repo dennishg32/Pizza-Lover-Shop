093b2fbd83e79f04ec22b9a625a8d722dc74c886
Dennis HG <dennishg32@gmail.com>
2020-10-10 13:54:47 +0200

    additional of another order

--- js/scripts.js
+++ js/scripts.js
@@ -29,3 +29,39 @@ Crust.prototype.CrustofPizza = function() {
 Toppings.prototype.ToppingsofPizza = function() {
     return this.name;
 };
+
+$(document).ready(function() {
+    $("#add-order").click(function() {
+        $("#new-orders").append(
+            '<div class="new-order">' +
+            '<div class="form-group">' +
+            '<label for="new-size">Size of Pizza</label>' +
+            '<select class="form-control" id="select-size" required>' +
+            "<option disabled selected hidden>-select size-</option>" +
+            "<option>small</option>" +
+            "<option>medium</option>" +
+            "<option>Large</option>" +
+            "</select>" +
+            "</div>" +
+            '<div class="form-group">' +
+            '<label for="new-crust">Crust</label>' +
+            '<select class="form-control" id="select-crust" required>' +
+            "<option disabled selected hidden>-select crust-</option>" +
+            "<option>crispy</option>" +
+            "<option>stuffed</option>" +
+            "<option>glutten-free</option>" +
+            "</select>" +
+            "</div>" +
+            '<div class="form-group">' +
+            '<label for="new-topping">Toppings</label>' +
+            '<select class="form-control" id="select-topping" required>' +
+            "<option disabled selected hidden>-select topping-</option>" +
+            "<option>pepperoni</option>" +
+            "<option>supreme</option>" +
+            "<option>hawaiian</option>" +
+            "</select>" +
+            "</div>" +
+            "</div>"
+        );
+    });
+});
