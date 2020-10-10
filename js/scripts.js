21b7eddd4317a9f7629de1a6ba8fff0267928eea
Dennis HG <dennishg32@gmail.com>
2020-10-10 13:33:47 +0200

    additional of prototype of functions

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,15 @@ function Toppings(name) {
     this.name = name;
     this.sizes = [];
 }
+Person.prototype.fullNames = function() {
+    return this.firstName + " " + this.lastName;
+};
+Size.prototype.SizeofPizza = function() {
+    return this.size + " " + this.price;
+};
+Crust.prototype.CrustofPizza = function() {
+    return this.name;
+};
+Toppings.prototype.ToppingsofPizza = function() {
+    return this.name;
+};
